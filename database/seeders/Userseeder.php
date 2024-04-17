1630ac067870a4ffa963c23cb58a33186abf0fc1
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-04-17 17:57:18 +0200

    initialize the view of the dashboard and databse

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@localhost',
+            'password' => Hash::make('admin123'),
+            'phone_number' => '1234567890',
+        ]);
+    }
+}
